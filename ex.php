8b861a7895a0a2fce7398e9fab0a5aef3edbf1ff
Tomas Celaya <tjcelaya@gmail.com>
2015-06-20 14:13:08 -0400

    begin inserting excessive debugging output

--- ex.php
+++ ex.php
@@ -3,4 +3,13 @@
 require_once("k.inc");
 
 $k=new K("localhost", 1234);
-var_dump($b=$k->k("a"));
+foreach ([
+    'T',
+    // 'kT',
+    // '() xkey kT',
+  ] as $expr) {
+    echo PHP_EOL.$expr.PHP_EOL."\033[33m=============\033[0m".PHP_EOL;
+    var_dump($T=$k->k($expr));
+}
+
+echo "\033[31m========================================\033[0m".PHP_EOL;
