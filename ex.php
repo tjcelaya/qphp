f04abae422dd1fe0f956e75d93bd10529c92f786
Tomas Celaya <tjcelaya@gmail.com>
2015-06-20 15:21:38 -0400

    blah

--- ex.php
+++ ex.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once("k.inc");
+require_once "k.inc";
 
-$k=new K("localhost", 1234);
+$k = new K("localhost", 1234);
 foreach ([
-    'T',
-    // 'kT',
-    // '() xkey kT',
-  ] as $expr) {
-    echo PHP_EOL.$expr.PHP_EOL."\033[33m=============\033[0m".PHP_EOL;
-    var_dump($T=$k->k($expr));
+  // 'T',
+	'kT',
+	// '() xkey kT',
+] as $expr) {
+	echo PHP_EOL . $expr . PHP_EOL . "\033[33m=============\033[0m" . PHP_EOL;
+	var_dump($T = $k->k($expr));
 }
 
-echo "\033[31m========================================\033[0m".PHP_EOL;
+echo "\033[31m========================================\033[0m" . PHP_EOL;
---
+++ test.q
@@ -0,0 +1,3 @@
+\p 1234
+T:([]a:enlist 2;b:enlist 3)
+kT:([a:enlist 2];b:enlist 3)
